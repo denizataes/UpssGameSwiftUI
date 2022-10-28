934b0348991239b2d3002a0244c332302a033294
Deniz Ata ES <denizataes@hotmail.com>
2022-10-28 15:17:33 +0300

    Initial Commit

---
+++ Party/PartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartyApp.swift
+//  Party
+//
+//  Created by Deniz Ata Eş on 28.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct PartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
